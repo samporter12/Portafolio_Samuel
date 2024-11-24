e1235ce0c3dc8b12b1636f957fea9067598c9066
samuel.guerrerosala <samuel.guerrerosala@campusucc.edu.co>
2024-11-24 10:48:30 -0500

    tercer intento de compilar en vercel

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation'
 
+{/* eslint-disable-next-line react/no-unescaped-entities */}
 
 const Introduction = () => {
     return (
