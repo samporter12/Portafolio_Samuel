16d2dd5e8fd7693e641a73e7a6d4ed10160a3902
samuel.guerrerosala <samuel.guerrerosala@campusucc.edu.co>
2024-11-24 13:42:17 -0500

    proyecto terminado

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -10,12 +10,12 @@ const ServicesPage = () => {
         <>
 
             <TransitionPage />
-            <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
+            <div className="grid items-center justify-center  py-20 h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
                 <div className="max-w-[450px]">
 
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">My <span className="font-bold text-secondary"> services.</span></h1>
-                    <p className="mb-3 text-xl text-gray-300">I offer frontend and backend development services specialized in creating attractive and functional websites and applications. Using the latest technologies, such as HTML, CSS and JavaScript, I design intuitive and responsive user interfaces that reflect my clients' brand identity and enhance their online presence.</p>
-                    <button className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contact</button>
+                    <p className="mb-3 text-xl flex text-gray-300">I offer frontend and backend development services specialized in creating attractive and functional websites and applications. Using the latest technologies, such as HTML, CSS and JavaScript, I design intuitive and responsive user interfaces that reflect my clients' brand identity and enhance their online presence.</p>
+                    <button className="px-3 flex py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contact</button>
                 </div>
 
                 {/* SLIDER */}
--- components/introduction.tsx
+++ components/introduction.tsx
@@ -6,11 +6,11 @@ import { TypeAnimation } from 'react-type-animation'
 
 const Introduction = () => {
     return (
-        <div className="z-20 w-full bg-darkBg/60">
-            <div className="z-20 grid items-center h-full px-60 py-20 md:py-0 md:grid-cols-2">
-                <Image src="/profile4.png" priority width="400" height="400" alt="Avatar" />
-                <div className="flex flex-col justify-center max-w-md">
-                    <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">On the way to a<br />
+        <div className="z-20 justify-center items-center w-full bg-darkBg/60">
+            <div className="z-20 grid items-center justify-center h-full px-40 py-20 md:py-0 md:grid-cols-2">
+                <Image src="/home-4.png" priority width="300" height="300" alt="Avatar" />
+                <div className=" justify-center items-center max-w-md">
+                    <h1 className="mb-5 flex flex-col text-2xl leading-tight justify-center text-center md:text-left md:text-4xl md:mb-10">On the way to a<br />
                         <TypeAnimation
                             sequence={[
                                 "optimized future",
@@ -30,16 +30,16 @@ const Introduction = () => {
                     </h1>
 
                     <p className="mx-auto mb-2 text-xl md:text-xl md:mx-0 md:mb-8">
-                    Hello everyone, Im Sam and Im very excited to know that you read this, now a little information about me, I live in Colombia, I am a university student. I have great ability to communicate with other people and work as a team and I have a B2 level in English and I would like to learn more and more for this.
+                    Hello everyone, Im Sam and Im very excited to know that you read this, now a little information about me, I live in Colombia, I am a university student. I have great ability to communicateand I have a B2 level in English.
                     </p>
 
                     <div className="flex items-center justify-center gap-3 md:justify-start md:gap-10">
-                        <a href="/portfolio" className="px-3 py-2 my-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50">
+                        <a href="/portfolio" className="px-3 flex py-2 my-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50">
                             Proyects
                         </a>
                         <a href="https://join.skype.com/invite/aTZBMQFCEdkQ"
-                            className="px-3 py-2 my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary" >
-                            Talk to Me
+                            className="px-3 py-2 flex my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary" >
+                            Talk
                         </a>
                     </div>
                 </div>
