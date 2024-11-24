4b67b542d4c4a6a759211045f4c134c9ef11cee5
samuel.guerrerosala <samuel.guerrerosala@campusucc.edu.co>
2024-11-24 11:00:38 -0500

    en vercel no compila

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation'
 
-{/* eslint-disable-next-line react/no-unescaped-entities */}
+
 
 const Introduction = () => {
     return (
@@ -30,7 +30,7 @@ const Introduction = () => {
                     </h1>
 
                     <p className="mx-auto mb-2 text-xl md:text-xl md:mx-0 md:mb-8">
-                    Hello everyone, I'm Sam and I'm very excited to know that you read this, now a little information about me, I live in Colombia, I am a university student very pleased to work with any of your guys or now talk to you. I have great ability to communicate with other people and work as a team, I have a B2 level in English and I would like to learn more and more for this wonderful job that is software developer.
+                    Hello everyone, Im Sam and Im very excited to know that you read this, now a little information about me, I live in Colombia, I am a university student very pleased to work with any of your guys or now talk to you. I have great ability to communicate with other people and work as a team, I have a B2 level in English and I would like to learn more and more for this wonderful job that is software developer.
                     </p>
 
                     <div className="flex items-center justify-center gap-3 md:justify-start md:gap-10">
